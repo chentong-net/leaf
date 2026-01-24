c41e0cae67f9d675d57094abc9cde8b595ea0ac8
chentong-net <contact@chentong.net>
2026-01-24 17:44:20 +0800

    移除无用依赖

--- app/src/main/java/net/chentong/leaf/demo/MainActivity.java
+++ app/src/main/java/net/chentong/leaf/demo/MainActivity.java
@@ -1,16 +1,15 @@
 package net.chentong.leaf.demo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import net.chentong.leaf.core.LeafView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // 直接创建并显示 SEngine 视图
+        // 直接创建并显示 Leaf 视图
         LeafView leafView = new LeafView(this);
         setContentView(leafView);
     }
