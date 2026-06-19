efcafbf718c10529092fb532cd0b83d8bb0ea991
chentong-net <contact@chentong.net>
2026-06-20 06:22:30 +0800

    iOS集成i18n、local_time插件

--- ios/App/App/ViewController.m
+++ ios/App/App/ViewController.m
@@ -10,11 +10,15 @@
 #import <Leaf_iOS/LFPluginRegistry.h>
 #import <File_Picker/FilePickerPlugin.h>
 #import <Path_Provider/PathProviderPlugin.h>
+#import <I18n/I18nPlugin.h>
+#import <Local_Time/LocalTimePlugin.h>
 
 @interface ViewController ()
 @property (nonatomic, strong) LeafView *leafView;
 @property (nonatomic, strong) FilePickerPlugin *filePickerPlugin;
 @property (nonatomic, strong) PathProviderPlugin *pathProviderPlugin;
+@property (nonatomic, strong) I18nPlugin *i18nPlugin;
+@property (nonatomic, strong) LocalTimePlugin *localTimePlugin;
 
 @end
 
@@ -25,8 +29,12 @@
 
     self.filePickerPlugin = [[FilePickerPlugin alloc] initWithPresentingViewController:self];
     self.pathProviderPlugin = [[PathProviderPlugin alloc] init];
+    self.i18nPlugin = [[I18nPlugin alloc] init];
+    self.localTimePlugin = [[LocalTimePlugin alloc] init];
     [[LFPluginRegistry sharedInstance] registerPlugin:self.filePickerPlugin];
     [[LFPluginRegistry sharedInstance] registerPlugin:self.pathProviderPlugin];
+    [[LFPluginRegistry sharedInstance] registerPlugin:self.i18nPlugin];
+    [[LFPluginRegistry sharedInstance] registerPlugin:self.localTimePlugin];
 
     self.leafView = [[LeafView alloc] initWithFrame:CGRectZero];
     self.leafView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -48,6 +56,8 @@
 - (void)dealloc {
     [[LFPluginRegistry sharedInstance] unregisterPlugin:@"FilePickerPlugin"];
     [[LFPluginRegistry sharedInstance] unregisterPlugin:@"PathProviderPlugin"];
+    [[LFPluginRegistry sharedInstance] unregisterPlugin:@"I18nPlugin"];
+    [[LFPluginRegistry sharedInstance] unregisterPlugin:@"LocalTimePlugin"];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
